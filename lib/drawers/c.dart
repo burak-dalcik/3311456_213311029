5dbb926e2040b087513c91795b5b3bcc9cfed8d2
burak-dalcik <burakdalcik2001@gmail.com>
2023-04-15 00:49:41 +0300

    first commit

---
+++ lib/drawers/c.dart
@@ -0,0 +1,106 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:obd1_213311029/sayfa1.dart';
+import 'package:obd1_213311029/sayfalar/Basketbol/basketgenel.dart';
+import 'package:obd1_213311029/sayfalar/Futbol/futbolgenel.dart';
+
+
+
+class CustomDrawer  extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //bool isNotifications = true;
+    return Drawer(
+      backgroundColor: Colors.transparent,
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.green.withOpacity(0.3),
+          ),
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: Image(
+                  image: AssetImage("images/giris.jpeg"),
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              _drawerItem(
+                  onTap: ()
+                  {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Futbol_Genel()));
+
+                  },
+                  iconData: CupertinoIcons.arrow_down_right,
+                  title:'FUTBOL'),
+              _drawerItem(
+                  onTap: ()
+                  {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Basket_Genel()));
+
+                  },
+                  iconData: CupertinoIcons.arrow_down_right,
+                  title:'BASKETBOL'),
+
+              _drawerItem(
+                  onTap: ()
+                  {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Ana_Sayfa()));
+
+                  },
+                  iconData: CupertinoIcons.arrow_down_right,
+                  title:'VOLEYBOL'),
+              _drawerItem(
+                  onTap: ()
+                  {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Ana_Sayfa()));
+
+                  },
+                  iconData: CupertinoIcons.antenna_radiowaves_left_right,
+                  title:'PERSONEL GİRİS'),
+              _drawerItem(
+                  onTap: ()
+                  {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Ana_Sayfa()));
+
+                  },
+                  iconData: CupertinoIcons.arrow_counterclockwise,
+                  title:'ANASAYFAYA DÖN'),
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  ListTile _drawerItem(
+      {required VoidCallback onTap,
+        required IconData iconData,
+        required String title,
+        Widget? trailing}) =>
+      ListTile(
+        onTap: onTap,
+        leading: Icon(
+          iconData,
+          color: Colors.white,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        trailing: trailing,
+      );
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:obd1_213311029/sayfa1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '12',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '12'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController? ad = TextEditingController();
+  TextEditingController? sifre = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('12 NUMARA'),
+        elevation: 10,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 470,
+                height: 370,
+                child: Image.asset('images/images.jpeg'),
+              ),
+
+              const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+
+                    decoration: InputDecoration(
+                      hintText: 'Kullanıcı Adınız',
+                      filled: true,
+                      fillColor: Colors.yellowAccent,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(35))),
+                    ),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+
+                    decoration: InputDecoration(
+                      hintText: 'Şifreniz',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(35))),
+                    ),
+                  )),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                    'Üye Girişi',
+                    style: TextStyle(fontSize: 24),
+                  )),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Ana_Sayfa()));
+                  },
+                  child: Text(
+                    'Ziyaretçi Girişi',
+                    style: TextStyle(fontSize: 24),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
